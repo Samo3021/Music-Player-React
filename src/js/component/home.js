2ac798d19d1cd3686562ba41619845ab027aefd3
Samo3021 <samuelcampos15009@gmail.com>
2021-03-06 20:00:33 +0000

    falta detalle

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -10,60 +10,53 @@ export class Home extends React.Component {
 		// this.cancionRef = React.createRef();
 		// console.log(this.cancionRef);
 
-		this.audio = null;
+		//this.audio = null;
 		this.state = {
-			currentIndex: 1,
-			songs: [
-				{
-					title: "South Park",
-					id: "south-park",
-					author: "Kyle",
-					url:
-						"https://assets.breatheco.de/apis/sound/files/cartoons/songs/south-park.mp3"
-				},
-				{
-					title: "Thunder Cats",
-					id: "thundercats",
-					author: "Moonra",
-					url:
-						"https://assets.breatheco.de/apis/sound/files/cartoons/songs/thundercats.mp3"
-				},
-				{
-					title: "X-Men",
-					id: "x-men",
-					author: "Profesor",
-					url:
-						"https://assets.breatheco.de/apis/sound/files/cartoons/songs/x-men.mp3"
-				}
-			]
+			fetchData: [],
+			currentSong: "",
+			currentIndex: 0
 		};
+		this.myAudioRef = React.createRef();
+		this.playNext = this.playNext.bind(this);
+		this.playBefore = this.playBefore.bind(this);
+		this.domain = "https://assets.breatheco.de/apis/sound/";
 	}
 
 	componentDidMount() {
-		this.pauseButton.style.display = "none";
 		fetch("https://assets.breatheco.de/apis/sound/songs")
 			.then(response => response.json())
-			.then(songs => {
-				this.setState({ songs });
+			.then(data => {
+				this.setState({ fetchData: data });
+				this.loadSong(this.domain + data[0].url, 0);
 			});
 		// console.log(this.fetchData);
 	}
 
-	changeSong(i) {
-		this.setState({ currentIndex: i });
-		this.audio.current.pause();
-		this.audio.current.load();
-		this.audio.currentIndex.play();
+	loadSong(url, index) {
+		this.setState({
+			currentSong: url,
+			currentIndex: index
+		});
+		this.myAudioRef.current.load();
 	}
-	play = i => {
-		let url = this.state.songs[i].url;
-		const songUrl = "https://assets.breatheco.de/apis/sound/" + url;
-		this.audio.src = songUrl;
-		this.audio.play();
-		this.playButton.style.display = "none";
-		this.pauseButton.style.display = "inline-block";
-		this.setState({ currentIndex: i });
-	};
+	playNext() {
+		const nextIndex = this.state.currentIndex + 1;
+		const nextSong =
+			this.state.fetchData[nextIndex] || this.state.fetchData[0];
+		this.playSong(this.domain + nextSong.url, nextIndex);
+	}
+
+	playBefore() {
+		const beforeIndex = this.state.currentIndex - 1;
+		const beforeSong =
+			this.state.fetchData[beforeIndex] ||
+			this.state.fetchData[this.state.fetchData.length - 1];
+		this.playSong(this.domain + beforeSong.url, beforeIndex);
+	}
+	playSong(url, index) {
+		this.loadSong(url, index);
+		this.myAudioRef.current.play();
+	}
 	pause = i => {
 		this.audio.pause();
 		this.playButton.style.display = "inline-block";
@@ -73,60 +66,49 @@ export class Home extends React.Component {
 	// handlePlay = () => {
 	// 	this.cancionRef.play();
 	// };
-	render() {
-		const musicList = this.state.songs.map((song, i) => {
-			return (
-				<li
-					className="shadow p-3 mb-5 bg-white rounded"
-					key={i}
-					onClick={() => this.play(this.state.currentIndex)}>
-					<span>{i + 1 + " "}</span>
-					<span>{song.name}</span>
-				</li>
-			);
-		});
 
-		const audioPlayer = (
-			<>
-				<div className="audioPlayer">
-					{/*<button className="">*/}
-					<button
-						className="btn btn-success"
-						onClick={() => this.play(this.state.currentIndex - 1)}>
-						<i className="fa fa-caret-left" aria-hidden="true" />
-					</button>
-					<button
-						className="btn btn-success"
-						ref={element => (this.playButton = element)}
-						onClick={() => this.play(this.state.currentIndex)}>
-						<i className="fa fa-play" aria-hidden="true" />
-					</button>
-					<button
-						className="btn btn-success"
-						ref={element => (this.pauseButton = element)}
-						onClick={() => this.pause(this.state.currentIndex)}>
-						<i className="fa fa-pause" aria-hidden="true" />
-					</button>
-					<button
-						className="btn btn-success"
-						onClick={() => this.play(this.state.currentIndex + 1)}>
-						<i className="fa fa-caret-right" aria-hidden="true" />
-					</button>
-					{/*</button>*/}
-				</div>
-				<audio ref={element => (this.audio = element)} />
-			</>
-		);
+	render() {
 		return (
-			<>
-				<h1 className="title">
-					<i className="fab fa-spotify"></i>8-Bit music list
-				</h1>
-				<div className="container">
-					<div id="lista">{musicList}</div>
+			<div className="title">
+				<h1>MUSIC PLAYER</h1>
+				<i class="fab fa-spotify"></i>
+				<div className="container ">
+					{this.state.fetchData.map((song, i) => {
+						const songURL = this.domain + song.url;
+						return (
+							<>
+								<div className="shadow p-3 mb-5 bg-white rounded">
+									<div key={i}>
+										<ul>
+											<li
+												onClick={() =>
+													this.playSong(songURL, i)
+												}>
+												<h3>{song.name}</h3>
+											</li>
+										</ul>
+									</div>
+								</div>
+							</>
+						);
+					})}
 				</div>
-				<div id="btn">{audioPlayer}</div>
-			</>
+				<div className="audioPlayer ">
+					<i
+						onClick={this.playBefore}
+						className="fas fa-2x mr-4 fa-step-backward"></i>
+					<audio controls ref={this.myAudioRef}>
+						<source
+							src={this.state.currentSong}
+							type="audio/mpeg"
+						/>
+					</audio>
+					<i
+						onClick={this.playNext}
+						className="fas fa-2x ml-4 fa-step-forward primary"></i>
+				</div>
+				<div></div>
+			</div>
 		);
 	}
 }
